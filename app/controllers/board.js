edc563253e2c993117ad6938ca6827d9073bc7dc
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-23 13:15:52 -0400

    clears out input box when new board is created

--- app/controllers/board.js
+++ app/controllers/board.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     newList() {
+      // alert(this.get("text"))
       this.get("model").pushObject({
         title: this.get("text")
       })
+      this.set("text", "")
     }
   }
 });

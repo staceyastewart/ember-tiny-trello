42d662861f35506f4291c483149dad9a0e14886f
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-23 20:50:25 -0400

    adds link with action attached

--- app/controllers/board.js
+++ app/controllers/board.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     newList() {
+      console.log("hitting")
       this.get("model").pushObject({
         id: (this.get("model").length + 1),
         title: this.get("text"),
@@ -14,6 +15,9 @@ export default Ember.Controller.extend({
       this.get("model")[listId-1].items.addObject(
         item
       )
+    },
+    addInput(){
+      console.log("HERE")
     }
   }
 });

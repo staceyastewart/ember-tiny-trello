d3451ef6aa9d8c07ee9fa4171bad0d9404f7b90c
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-26 12:30:43 -0400

    fixes modal bug

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  modalOpen: false,
+  // modalOpen: false,
   actions: {
     openModal() {
-      console.log("this")
-      console.log(this)
-      console.log(this.get("item"))
-      this.set('modalOpen', true);
+      // console.log("this")
+      // console.log(this)
+      // console.log(this.get("item"))
+      // this.set('modalOpen', true);
     }
   }
 });
--- app/components/modal-component.js
+++ app/components/modal-component.js
@@ -1,20 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  modalOpen: false,
   actions: {
     closeModal(){
       this.set("modalOpen", false)
-    },
-    saveEditedItem(){
-      // save item from form once edited
-      // this.sendAction("rerenderModal")
     },
     confirmDelete() {
       const confirmationOfDelete = confirm('Are you sure you want to delete this item?');
       if (confirmationOfDelete) {
         this.sendAction("deleteItem", this.get("listId"), this.get("item.id"))
-        this.send('closeModal');
+        this.toggleProperty("modalOpen")
       }
     }
   }

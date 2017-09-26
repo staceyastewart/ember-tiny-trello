4e7bc2057b18b436aa7465bb97e72b44be692de5
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-26 01:12:15 -0400

    form on modal edits model data but does not rerender on page

--- app/components/form-component.js
+++ app/components/form-component.js
@@ -3,15 +3,30 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     sendDataToController() {
+      console.log(this.get("text"))
+      console.log(this.get("listId"))
+      console.log(this.get("itemId"))
+      // not hitting first if when the item is added to the page
       if(this.get("listId")){
-        // adding item to list
-        this.attrs.newItem(this.get("listId"), this.get("text"))
+        if(this.get("itemId")){
+          // edit
+          console.log("EDITING ITEM")
+          // set if for title and for description
+          this.sendAction("editItem", this.get("listId"), this.get("itemId"), this.get("text"), "description")
+        } else {
+          // adding item to list
+          console.log("why are you trying to add a list")
+          this.attrs.newItem(this.get("listId"), this.get("text"))
+        }
       } else {
         // adding a list
         this.sendAction("newList", this.get("text"));
       }
       this.set("text", "")
       this.sendAction("toggleIsAdding")
+    },
+    editEachItem(){
+
     }
   }
 });
--- app/components/modal-component.js
+++ app/components/modal-component.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     closeModal(){
       this.set("modalOpen", false)
+    },
+    saveEditedItem(){
+      // save item from form once edited
     }
   }
 });

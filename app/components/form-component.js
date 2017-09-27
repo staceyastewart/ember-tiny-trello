605a9801db5063681a762a85f851d659c6fd040b
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-27 11:26:34 -0400

    updates styling

--- app/components/form-component.js
+++ app/components/form-component.js
@@ -3,15 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     sendDataToController() {
-      if(this.get("listId")){
-        // adding item to list
-        this.attrs.newItem(this.get("listId"), this.get("text"))
-      } else {
-        // adding a list
-        this.sendAction("newList", this.get("text"));
+      if(this.get("text")){ //if the user entered data
+        if(this.get("listId")){
+          // adding item to list
+          this.attrs.newItem(this.get("listId"), this.get("text"))
+        } else {
+          // adding a list
+          this.sendAction("newList", this.get("text"));
+        }
+          this.set("text", "")
+          this.sendAction("toggleIsAdding")
+        } else { //user did not enter data
+          alert("You did not enter any text!")
+        }
       }
-        this.set("text", "")
-        this.sendAction("toggleIsAdding")
-    }
   }
 });

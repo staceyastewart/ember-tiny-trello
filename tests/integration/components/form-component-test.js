bb7299e937ab7116412540caca69f28dda9ec1b7
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-09-28 09:59:31 -0400

    form component tests

--- tests/integration/components/form-component-test.js
+++ tests/integration/components/form-component-test.js
@@ -1,6 +1,25 @@
-// import { moduleForComponent, test } from 'ember-qunit';
-// import hbs from 'htmlbars-inline-precompile';
-//
-// moduleForComponent('form-component', 'Integration | Component | form component', {
-//   integration: true
-// });
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+let items = Ember.Object.create({
+      id: 1,
+      title: "Title",
+      description: "Long description"
+})
+
+moduleForComponent('form-component', 'Integration | Component | form component', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.set("item", items)
+  this.set("listId", "1")
+  // this.set("description", item.description)
+
+  this.render(hbs`{{form-component text=item.description listId=listId itemId=item.id}}`);
+
+  assert.equal(this.get("listId"), "1");
+  assert.equal(this.get("item.id"), "1");
+  assert.equal(this.get("item.description"), "Long description");
+});
